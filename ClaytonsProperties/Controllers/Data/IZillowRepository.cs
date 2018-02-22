a06717da17b9ea684c0ea956b5338725b966e668
Clayton <clayton.rothschild@parivedasolutions.com>
2018-02-22 17:08:13 -0600

    Added interface scaffolding for Zillow. Arranged some project files.

---
+++ ClaytonsProperties/Controllers/Data/IZillowRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ClaytonsProperties.Controllers.Data
+{
+    public interface IZillowRepository
+    {
+        Task<object> GetPropertyAsync(int propertyId);
+    }
+}
---
+++ ClaytonsProperties/Controllers/Data/Impl/ZillowRepository.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ClaytonsProperties.Controllers.Data.Impl
+{
+    public class ZillowRepository : IZillowRepository
+    {
+        private static string baseUrl = "";
+
+        public async Task<object> GetPropertyAsync(int property)
+        {
+            var userString = await GetStringAsync(baseUrl + "properties/" + property);
+            // Here I use Newtonsoft.Json to deserialize JSON string to User object
+            var user = JsonConvert.DeserializeObject<object>(userString);
+            return user;
+        }
+
+        private static async Task<string> GetStringAsync(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                return await httpClient.GetStringAsync(url);
+            }
+        }
+    }
+}
