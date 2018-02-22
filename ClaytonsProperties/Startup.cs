eefffdbe989ccf0397a8c5aba79136efd3ac94b3
Clayton <clayton.rothschild@parivedasolutions.com>
2018-02-22 16:29:34 -0600

    Add project files.

---
+++ ClaytonsProperties/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClaytonsProperties.Startup))]
+namespace ClaytonsProperties
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
